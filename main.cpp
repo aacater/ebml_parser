f4ae8c733e85cbe39c841f7f8ad72277ed563ba7
aacater <aacater@alaska.edi>
2019-03-20 11:47:42 -0800

    added more comments

--- main.cpp
+++ main.cpp
@@ -8,20 +8,33 @@
 #include <chrono>
 #include <iomanip>
 
-#include <unistd.h>
+#include <unistd.h> // requires unix
 #include <time.h>
 
-#define BUFSIZE 1048576 // 1MB
 
-enum ebml_element_type {
-	MASTER,
-	UINT,
-	INT,
-	STRING,
-	UTF8,
-	BINARY,
-	FLOAT,
-	DATE
+using std::cout;
+using std::string;
+using std::endl;
+using std::hex;
+using std::ostream;
+using std::array;
+using std::bitset;
+using std::dec;
+using std::fixed;
+using std::time_t;
+using std::strcmp;
+
+#define BUFSIZE 1048576 // 1MB = 1024^2
+
+enum ebml_element_type { // posible data types
+	MASTER,	// other EBL sub-elemnts of next lower level
+	UINT,	// unsigned integer, 1 to 8 bytes
+	INT,	// signed integer, 1 to 8 bytes
+	STRING,	// ASCII characters 0x20 to 0x7E
+	UTF8,	// unicode, padded with zeros
+	BINARY,	// not parsed
+	FLOAT,	// big-endian, 4 and 8 bytes (32 or 64 bits)
+	DATE	// signed 8 byte integer in nanoseconds
 };
 
 class simple_vint{
@@ -29,7 +42,7 @@ public:
 	uint8_t width;
 	uint8_t data[8];
 
-	bool is_all_ones(){
+	bool is_all_ones(){ // check if data is 255 (0xFF )
 		for(int i = 1; i < width - 1; i++){
 			if(data[i] != 255){
 				return false;
@@ -38,7 +51,7 @@ public:
 		return true;
 	}
 
-	uint64_t get_uint(){
+	uint64_t get_uint(){ // convert data to unsigned into - big endian
 		uint64_t value = 0;
 		value = data[width - 1];
 		for(int i = width - 1; i > 0; --i){
@@ -46,25 +59,15 @@ public:
 		}
 		return value;
 	}
-
-	// Unused, good point though.
-	uint64_t get_little_endian_uint(){
-		uint64_t value = 0;
-		value = data[0];
-		for(int i = 0; i < width - 1; ++i){
-			value += ((uint64_t)data[i + 1] << (i * 8));
-		}
-		return value;
-	}
 };
 
-class ebml_element{
+class ebml_element{ // ebml element defined using data from spec.cpp
 public:
-	std::string name;
-	std::array<uint8_t, 4> id;
+	string name;
+	array<uint8_t, 4> id;
 	enum ebml_element_type type;
 
-	ebml_element(std::string name, std::array<uint8_t, 4> const& id, enum ebml_element_type type)
+	ebml_element(string name, array<uint8_t, 4> const& id, enum ebml_element_type type)
 	:name(name), id(id), type(type){}
 };
 
@@ -72,34 +75,36 @@ const int SPEC_LEN = 250; // number of types of ebml elements
 
 #include "spec.cpp" // include embl element data strcture
 
-ebml_element* get_element(std::array<uint8_t, 4> id, uint8_t level){
+// compares data to spec.cpp to find which ebml element the data is
+ebml_element* get_element(array<uint8_t, 4> id, uint8_t level){
 	bool found;
 	for(int i = 0; i < SPEC_LEN; ++i){
 		found = true;
-		for(int j = 0; j < level; ++j){
+		for(int j = 0; j < level; ++j){ // check for matching id
 			if(ebml_spec[i]->id[j] != id[j]){
 				found = false;
 				break;
 			}
 		}
-		if(found){
+		if(found){	// if found return the element info
 			return ebml_spec[i];
 		}
 	}
 	return 0;
 }
-
+// logging stuff
+// if verbose = true then prints bits of data
 bool verbose = false;
-typedef std::ostream& (*manip) (std::ostream&);
+typedef ostream& (*manip) (ostream&);
 class verbose_log{};
 template <class T> verbose_log& operator<< (verbose_log& l, const T& x){
 	if(verbose)
-		std::cout << x;
+		cout << x;
 	return l;
 }
 verbose_log& operator<< (verbose_log& l, manip manipulator){
 	if(verbose)
-		std::cout << manipulator;
+		cout << manipulator;
 	return l;
 }
 verbose_log vlog;
@@ -109,26 +114,26 @@ public:
 void parse(int fd){
 	int len, mask, pos = 0;
 	uint8_t buffer[BUFSIZE];
-	std::bitset<8> bits;
+	bitset<8> bits;
 
 	while(1){
 		// Get EBML Element ID first byte.
-		if((len = read(fd, buffer, 1)) < 0){
-			std::cout << "Uh oh, read first id byte error!\n";
+		if((len = read(fd, buffer, 1)) < 0){						// check if read length is less than zero
+			cout << "Uh oh, read first id byte error!\n";		// exit if so
 			break;
-		}else if(len == 0){
-			std::cout << "DONE!" << std::endl;
+		}else if(len == 0){											// check if read length is zero
+			cout << "DONE!" << endl;						// reached end of file
 			break;
 		}
-		pos++;
+		pos++;														// increment pos
 
-		if(buffer[0] == 0){
-			std::cout << "Read '0' byte..." << std::endl;
+		if(buffer[0] == 0){											// skip until buffer is non zero
+			cout << "Read '0' byte..." << endl;
 			continue;
 		}
 
-		bits = std::bitset<8>(buffer[0]);
-		vlog << "Element ID First Byte: " << bits << std::endl;
+		bits = bitset<8>(buffer[0]);
+		vlog << "Element ID First Byte: " << bits << endl;
 
 		simple_vint id;
 		id.width = 1;
@@ -142,7 +147,7 @@ void parse(int fd){
 		id.data[0] = buffer[0];
 		// Get EBML Element ID vint data.
 		if((len = read(fd, buffer, id.width - 1)) != id.width - 1){
-			std::cout << "Uh oh, read id data error!\n";
+			cout << "Uh oh, read id data error!\n";
 			break;
 		}
 		pos += id.width - 1;
@@ -150,27 +155,27 @@ void parse(int fd){
 		// Get EBML Element ID.
 		for(int i = 1; i < id.width; ++i){
 			id.data[i] = buffer[i - 1];
-			bits = std::bitset<8>(buffer[i]);
+			bits = bitset<8>(buffer[i]);
 			vlog << ' ' << bits;
 		}
-		vlog << std::endl;
+		vlog << endl;
 		if(verbose){
 			vlog << "Element ID: 0x";
 			for(int i = 0; i < id.width; ++i){
-				vlog << std::hex << (int)id.data[i];
+				vlog << hex << (int)id.data[i];
 			}
-			vlog << std::endl;
+			vlog << endl;
 		}
 
 		// Get EBML Element Size first byte.
 		if((len = read(fd, buffer, 1)) != 1){
-			std::cout << "Uh oh, read first size byte error!\n";
+			cout << "Uh oh, read first size byte error!\n";
 			break;
 		}
 		pos++;
 
-		bits = std::bitset<8>(buffer[0]);
-		vlog << "Element Size First Byte: " << bits << std::endl;
+		bits = bitset<8>(buffer[0]);
+		vlog << "Element Size First Byte: " << bits << endl;
 
 		simple_vint size;
 		size.width = 1;
@@ -185,56 +190,57 @@ void parse(int fd){
 		size.data[0] = buffer[0];
 		// Get EBML Element Size vint data.
 		if((len = read(fd, buffer, size.width - 1)) != size.width - 1){
-			std::cout << "Uh oh, read id data error!\n";
+			cout << "Uh oh, read id data error!\n";
 			break;
 		}
 		pos += size.width - 1;
-		bits = std::bitset<8>(size.data[0]);
+		bits = bitset<8>(size.data[0]);
 		vlog << "Element Size Bytes: " << bits;
 		// Get EBML Element Size.
 		for(int i = 1; i < size.width; ++i){
 			size.data[i] = buffer[i - 1];
-			bits = std::bitset<8>(buffer[i]);
+			bits = bitset<8>(buffer[i]);
 			vlog << ' ' << bits;
 		}
-		vlog << std::endl;
-		vlog << "Element Size: " << std::dec << size.get_uint() << std::endl;
+		vlog << endl;
+		vlog << "Element Size: " << dec << size.get_uint() << endl;
 
 		// Specification for ID lookup.
 		ebml_element* e = get_element(
 			{{id.data[0], id.data[1], id.data[2], id.data[3]}},
 			id.width);
 
-		vlog << "--------------------------------------------------------" << std::endl;
+		vlog << "--------------------------------------------------------" << endl;			// after this line the parser prints
+																							// the data based on the ebml element type
 		if(e != 0){
 			if(e->type != MASTER){
 				// Get EBML Element Data, parse it.
 				uint64_t data_len = size.get_uint();
 				if((len = read(fd, buffer, data_len) != data_len)){
-					std::cout << "Uh oh, could not read all the data!" << std::endl;
-					std::cout << "Wanted " << data_len << " found " << len << std::endl;
+					cout << "Uh oh, could not read all the data!" << endl;
+					cout << "Wanted " << data_len << " found " << len << endl;
 					break;
 				}
 				pos += data_len;
-				std::cout << '(' << std::dec << pos << ") " << e->name << ": ";
+				cout << '(' << dec << pos << ") " << e->name << ": ";
 				if(e->type == STRING || e->type == UTF8){
 					for(int i = 0; i < data_len; ++i){
-						std::cout << buffer[i];
+						cout << buffer[i];
 					}
-					std::cout << std::endl;
+					cout << endl;
 				}else if(e->type == BINARY){
 					for(int i = 0; i < data_len; ++i){
 						// I'll only care about the first 32 binary bytes.
 						if(i == 32 && !verbose){
-							std::cout << "...";
+							cout << "...";
 							break;
 						}
-						std::cout << std::hex << (int)buffer[i];
+						cout << hex << (int)buffer[i];
 					}
-					std::cout << std::endl;
+					cout << endl;
 					if(e->name == "SimpleBlock" || e->name == "Block"){
-						bits = std::bitset<8>(buffer[0]);
-						vlog << "Block First Byte: " << bits << std::endl;
+						bits = bitset<8>(buffer[0]);
+						vlog << "Block First Byte: " << bits << endl;
 						simple_vint track_number;
 						track_number.width = 1;
 						mask = 0x80;
@@ -243,15 +249,15 @@ void parse(int fd){
 							track_number.width++;
 						}
 						buffer[0] ^= mask;
-						vlog << "Block Track Number Bytes: " << std::endl;
+						vlog << "Block Track Number Bytes: " << endl;
 						for(int i = 0; i < track_number.width; ++i){
-							bits = std::bitset<8>(buffer[i]);
+							bits = bitset<8>(buffer[i]);
 							vlog << bits << ' ';
 							track_number.data[i] = buffer[i];
 						}
-						std::cout << "Track Number: " << std::dec << (int)track_number.get_uint() << std::endl;
+						cout << "Track Number: " << dec << (int)track_number.get_uint() << endl;
 						int16_t timecode = (int16_t)(((uint16_t)buffer[track_number.width] << 8) | buffer[track_number.width + 1]);
-						std::cout << "Timecode: " << std::dec << (int)timecode << std::endl;
+						cout << "Timecode: " << dec << (int)timecode << endl;
 					}
 				}else if(e->type == UINT){
 					simple_vint data;
@@ -261,17 +267,17 @@ void parse(int fd){
 						data.width++;
 					}
 					uint64_t val = data.get_uint();
-					std::cout << std::dec << val;
+					cout << dec << val;
 					if(e->id[0] == 0x83){
 						if(val == 1){
-							std::cout << " (video)";
+							cout << " (video)";
 						}else if(val == 2){
-							std::cout << " (audio)";
+							cout << " (audio)";
 						}else if(val == 3){
-							std::cout << " (complex)";
+							cout << " (complex)";
 						}
 					}
-					std::cout << std::endl;
+					cout << endl;
 				}else if(e->type == FLOAT){
 					simple_vint data;
 					data.width = 0;
@@ -283,15 +289,15 @@ void parse(int fd){
 					if(data.width == 4){
 						float float_val;
 						memcpy(&float_val, &int_val, 4);
-						std::cout << std::fixed << std::dec << float_val;
+						cout << fixed << dec << float_val;
 					}else if(data.width == 8){
 						double double_val;
 						memcpy(&double_val, &int_val, 8);
-						std::cout << std::fixed << std::dec << double_val;
+						cout << fixed << dec << double_val;
 					}else{
-						std::cout << "Bad float width:: " << std::dec << (int)data.width;
+						cout << "Bad float width:: " << dec << (int)data.width;
 					}
-					std::cout << std::endl;
+					cout << endl;
 				}else if(e->type == DATE){
 					simple_vint data;
 					data.width = 0;
@@ -303,11 +309,11 @@ void parse(int fd){
 					// time point 0 (Jan 1 1970)
 					// nanoseconds to millenium (Jan 1 2001)
 					// nanoseconds to date file was created.
-					std::time_t date_val = std::chrono::system_clock::to_time_t(std::chrono::system_clock::time_point::time_point() + std::chrono::nanoseconds(978307200000000000) + std::chrono::nanoseconds(int64_t(data.get_uint())));
+					time_t date_val = std::chrono::system_clock::to_time_t(std::chrono::system_clock::time_point::time_point() + std::chrono::nanoseconds(978307200000000000) + std::chrono::nanoseconds(int64_t(data.get_uint())));
 					// This is the local time; PST in San Francisco, -7h or something.)
-					//std::cout << std::ctime(&date_val);
+					//cout << ctime(&date_val);
 					// This is the UTC time.
-					std::cout << asctime(gmtime(&date_val));
+					cout << asctime(gmtime(&date_val));
 				}else if(e->type == INT){
 					simple_vint data;
 					data.width = 0;
@@ -315,34 +321,34 @@ void parse(int fd){
 						data.data[i] = buffer[i];
 						data.width++;
 					}
-					std::cout << std::dec << int64_t(data.get_uint()) << std::endl;
+					cout << dec << int64_t(data.get_uint()) << endl;
 				}else{
 					for(int i = 0; i < data_len; ++i){
-						std::cout << std::hex << (int)buffer[i];
+						cout << hex << (int)buffer[i];
 					}
-					std::cout << std::endl;
+					cout << endl;
 				}
 			}else{
 				// Master data is actually just more elements, continue.
-				std::cout << '(' << std::dec << pos << ')' << " ----- " << e->name << " [";
+				cout << '(' << dec << pos << ')' << " ----- " << e->name << " [";
 				if(size.is_all_ones()){
-					std::cout << "Unknown";
+					cout << "Unknown";
 				}else{
-					std::cout << size.get_uint();
+					cout << size.get_uint();
 				}
-				std::cout << ']' << std::endl;
+				cout << ']' << endl;
 			}
 		}else{
-			std::cout << "UNKNOWN ELEMENT!" << std::endl;
+			cout << "UNKNOWN ELEMENT!" << endl;
 		}
-		vlog << "--------------------------------------------------------" << std::endl;
+		vlog << "--------------------------------------------------------" << endl;
 	}
 }
 };
 
 int main(int argc, char** argv){
 	for(int i = 0; i < argc; i++){
-                if(std::strcmp(argv[i], "-v") == 0){
+                if(strcmp(argv[i], "-v") == 0){
                         verbose = true;
                 }
 	}
